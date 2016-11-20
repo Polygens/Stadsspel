3b44563c8e7575add033ab11858a4011ae6c0b5e
Polygens <willem.gillis@gmail.com>
2016-11-20 12:18:55 +0100

    Replace MovePlayer

--- Stadsspel Sint-Niklaas/Assets/Scripts/Network/SetupLocalPlayer.cs
+++ Stadsspel Sint-Niklaas/Assets/Scripts/Network/SetupLocalPlayer.cs
@@ -2,49 +2,46 @@
 using System.Collections;
 using UnityEngine.Networking;
 
-public class SetupLocalPlayer : NetworkBehaviour {
+public class SetupLocalPlayer : NetworkBehaviour
+{
 
-    [SyncVar] //From server to client
-    public string pName = "player";
+	[SyncVar] //From server to client
+	public string pName = "player";
 
-    [SyncVar]
-    public Color playerColor = Color.white;
+	[SyncVar]
+	public Color playerColor = Color.white;
 
-    void OnGUI()
-    {
-        if(isLocalPlayer)
-        {
-            pName = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), pName);
-            if (GUI.Button(new Rect(130, Screen.height - 40, 80, 30), "Change"))
-            {
-                CmdChangeName(pName);
-            }
-        }       
-    }
+	void OnGUI()
+	{
+		if (isLocalPlayer) {
+			pName = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), pName);
+			if (GUI.Button(new Rect(130, Screen.height - 40, 80, 30), "Change")) {
+				CmdChangeName(pName);
+			}
+		}
+	}
 
-    [Command] //Tells Unity function will be called on the server
-    public void CmdChangeName(string newName)
-    {
-        pName = newName;
-    }
-	
-	void Start ()
-    {
-	    if(isLocalPlayer)
-        {
-            GetComponent<MovePlayer>().enabled = true;
-        }
+	[Command] //Tells Unity function will be called on the server
+	public void CmdChangeName(string newName)
+	{
+		pName = newName;
+	}
 
-        Renderer[] rends = GetComponentsInChildren<Renderer>();
-        foreach (Renderer r in rends)
-        {
-            r.material.color = playerColor;
-        }
+	void Start()
+	{
+		if (isLocalPlayer) {
+			GetComponent<MoveAvatar>().enabled = true;
+		}
 
-    }
+		Renderer[] rends = GetComponentsInChildren<Renderer>();
+		foreach (Renderer r in rends) {
+			r.material.color = playerColor;
+		}
 
-    void Update()
-    {
-        GetComponentInChildren<TextMesh>().text = pName;
-    }
+	}
+
+	void Update()
+	{
+		GetComponentInChildren<TextMesh>().text = pName;
+	}
 }
