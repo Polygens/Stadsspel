2b5c61d209fd6ac11d837aded951d4817223a566
SeppePeelman <Seppe.P@hotmail.com>
2016-12-22 14:19:19 +0100

    BankAccounts start

---
+++ Stadsspel Sint-Niklaas/Assets/Scripts/Elements/Buildings/Bank/Bank.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public class Bank : Financial
+{
+    private List<BankAccount> bankAccounts = new List<BankAccount>();
+
+	public Bank()
+	{
+		throw new System.NotImplementedException();
+        
+	}
+
+    public override void GainMoneyOverTime()
+    {
+        // Interest
+
+        base.GainMoneyOverTime();
+    }
+
+    public BankAccount CreateBankAccount(TeamID id)
+    {
+        return new BankAccount(id);
+    }
+}
---
+++ Stadsspel Sint-Niklaas/Assets/Scripts/Elements/Buildings/Bank/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿public class BankAccount
+{
+    public TeamID teamID;
+    private int balance;
+
+    public BankAccount(TeamID id)
+    {
+        teamID = id;
+        balance = 0;
+    }
+
+    public int Balance
+    {
+        get { return balance; }
+        set { balance = value; }
+    }
+
+    public void Transaction(int amount)
+    {
+        balance += amount;
+    }
+
+}
