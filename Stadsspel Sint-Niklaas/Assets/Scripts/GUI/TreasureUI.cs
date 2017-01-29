8079cb19efd60ce7bb400288e84b1101a596d85f
Yannis Tack <yannis.tack@hotmail.com>
2017-01-29 16:13:30 +0100

    Added TreasureUI script + can open the chest ui now

---
+++ Stadsspel Sint-Niklaas/Assets/Scripts/GUI/TreasureUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TreasureUI : MonoBehaviour
+{
+    public Text amountOfOwnMoney;
+    public Text amountOfChestMoney;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    private void OnEnable()
+    {
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        amountOfOwnMoney.text = GameManager.s_Singleton.Player.Person.AmountOfMoney.ToString();
+        
+    }
+}
