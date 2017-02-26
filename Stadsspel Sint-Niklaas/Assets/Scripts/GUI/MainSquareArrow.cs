509e44856d3c88afa7785a4e29e9377ae341144d
SeppePeelman <Seppe.P@hotmail.com>
2017-02-26 15:40:28 +0100

    Added toggleable arrow around the player that points to the Head District (Bug: cant find head district)

---
+++ Stadsspel Sint-Niklaas/Assets/Scripts/GUI/MainSquareArrow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSquareArrow : MonoBehaviour
+{
+    HeadDistrict headDistrict;
+
+	void Start ()
+    {
+        headDistrict = FindObjectOfType<HeadDistrict>();
+    }
+	
+
+	void Update ()
+    {
+        //transform.LookAt(headDistrict.transform);
+	}   
+}
