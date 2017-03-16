a13f97035e4f1e5101e98b498efe326ae2a77b42
Polygens <willem.gillis@gmail.com>
2017-03-16 20:20:51 +0100

    Fix responsive UI user panel and added log button

--- Stadsspel Sint-Niklaas/Assets/Scripts/Game/GUI/LogUI.cs
+++ Stadsspel Sint-Niklaas/Assets/Scripts/Game/GUI/LogUI.cs
@@ -3,4 +3,10 @@
 public class LogUI : MonoBehaviour
 {
 
+	public void ToggleLogsPanel(bool newState)
+	{
+		for(int i = 0; i < transform.childCount; i++) {
+			transform.GetChild(i).gameObject.SetActive(newState);
+		}
+	}
 }
