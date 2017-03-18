748ffa0129d276762fd1092d478787528d674d01
SeppePeelman <Seppe.P@hotmail.com>
2017-03-18 20:53:47 +0100

    Added scoreboard UI

---
+++ Stadsspel Sint-Niklaas/Assets/Scripts/Game/GUI/FinalScoreUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScoreUI : MonoBehaviour
+{
+    public GameObject scoreEntryPrefab;
+    public Transform content;
+
+    private List<Team> teamOrder = new List<Team>();
+
+    private void OnEnable()
+    {
+        SortTeams();
+
+        for (int i = 0; i < teamOrder.Count; i++)
+        {
+            GameObject scoreEntryInstance = Instantiate(scoreEntryPrefab, content);
+            scoreEntryInstance.transform.FindChild("Place").GetComponent<Text>().text = (i + 1).ToString();
+            scoreEntryInstance.transform.FindChild("Team").GetComponent<Image>().color = TeamData.GetColor(teamOrder[i].TeamID);
+            scoreEntryInstance.transform.FindChild("Money").GetComponent<Text>().text = teamOrder[i].TotalMoney.ToString();
+        }
+    }
+
+    private void SortTeams()
+    {
+        for (int i = 0; i < GameManager.s_Singleton.Teams.Length; i++)
+        {
+            teamOrder.Add(GameManager.s_Singleton.Teams[i]);
+        }
+
+        teamOrder.Sort(SortByTotalMoney);
+    }
+
+    static int SortByTotalMoney(Team t1, Team t2)
+    {
+        return t1.TotalMoney.CompareTo(t2.TotalMoney);
+    }
+}
