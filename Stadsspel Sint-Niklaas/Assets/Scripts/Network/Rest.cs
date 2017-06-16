dc6c63774f0d9e92c632ef2ac56de0611633ded8
gaston_KDG <gaston.lhoest@student.kdg.be>
2017-06-16 19:49:47 +0200

    B0015

--- Stadsspel Sint-Niklaas/Assets/Scripts/Network/Rest.cs
+++ Stadsspel Sint-Niklaas/Assets/Scripts/Network/Rest.cs
@@ -11,8 +11,8 @@ using System.Text;
 public class Rest
 {
 	//private const string BASE_URL = "https://stniklaas-stadsspel.herokuapp.com/api/"; todo deprecated
-	//private const string BASE_URL = "https://stadspelapp-sintniklaas.herokuapp.com//api/";
-	private const string BASE_URL = "http://localhost:8090/api/";
+	private const string BASE_URL = "https://stadspelapp-sintniklaas.herokuapp.com/api/";
+	//private const string BASE_URL = "http://localhost:8090/api/";
 	private const string GAME_SUFFIX = "games";
 	private const string COLOR_SUFFIX = "colors";
 	private const string ACCOUNT_SUFFIX = "accounts";
