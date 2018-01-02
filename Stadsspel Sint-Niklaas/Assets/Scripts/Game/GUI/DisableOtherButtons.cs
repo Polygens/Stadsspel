c5ff1196230dd42c95e58d930299dbfdfb5cdb4e
Unknown <jarnohermans94@gmail.com>
2018-01-02 21:15:13 +0100

    QuickFix UI

---
+++ Stadsspel Sint-Niklaas/Assets/Scripts/Game/GUI/DisableOtherButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOtherButtons : MonoBehaviour {
+
+	public List<GameObject> buttons = new List<GameObject>();
+
+  private void OnEnable() {
+		foreach (GameObject button in buttons)
+		{
+			button.SetActive(false);
+		}
+  }
+	
+}
